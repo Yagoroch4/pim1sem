ac35d300b947e1aea8ee105f8c10c6cf1c27c4de
Yagoroch4 <yago@vrmoraes.com>
2022-10-12 13:50:30 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <string.h>
+
+typedef struct{
+char nome[30], dia[3], mes[3], ano[5];
+float valor;
+}c_pagar;
+
+typedef struct{
+char nome[30], data[10];
+float valor;
+}c_receber;
+
+int main(){
+
+    setlocale (LC_ALL, "portuguese");
+    int tam = 5, i = 0, cp = 0, op, retorno;
+    char resposta;
+    float total_desp = 0;
+    c_pagar pagar[100];
+    c_receber receber[100];
+
+
+    do{
+        printf("----------------------------------------------\n");
+        printf("------CFA - CONTROLE DE FLUXO AUTOMOTIVO------\n");
+        printf("----------------------------------------------\n\n");
+        printf("\n---------------CADASTROS--------------------\n\n");
+        printf("[1] CADASTRAR CONTAS A PAGAR\n");
+        printf("[2] CONSULTAR CONTAS A PAGAR\n");
+        printf("\nEscolha uma opção:");
+        scanf("%d", &op);
+
+        switch(op){
+            case 1:     // o usuario cadastra contas a pagar do cliente
+            printf("Olá, para efetuar um cadastro de contas a pagar digite os dados abaixo: \n");
+            do{
+                fflush(stdin);
+                printf("Digite o nome da fornecedor: \n");
+                gets(pagar[i].nome);
+                fflush(stdin);
+                printf("Insira o dia de vencimento da conta: \n");
+                scanf("%s", &pagar[i].dia);
+                fflush(stdin);
+                printf("Insira mes de vencimento da conta: \n");
+                scanf("%s", &pagar[i].mes);
+                fflush(stdin);
+                printf("Insira ano de vencimento da conta: \n");
+                scanf("%s", &pagar[i].ano);
+                fflush(stdin);
+                printf("digite o valor da conta: \n");
+                scanf("%f", &pagar[i].valor);
+                fflush(stdin);
+                printf("\nDeseja fazer outro cadastro (s/n)?: ");
+                resposta = getche();
+                resposta = tolower(resposta); // converte a resposta do usuário para um caractere minúsculo
+                printf("\n\n");
+                fflush(stdin);
+                i++;
+                cp++;
+
+            }while(resposta == 's' && i < tam);
+        break;
+
+        case 2:{
+        char mes[3];
+
+            if (cp == 0)
+                printf("A empresa não possui contas a pagar");
+
+            else{
+                fflush(stdin);
+                printf("\nInforme o mês: ");
+                scanf("%s", &mes);
+                for (i = 0; i < cp; i++){
+                    retorno = strcmp(pagar[i].mes,mes); // compara a string e for igual resultado será 0
+                    if (retorno == 0){
+                        printf("\nFornecedor:%s \n", pagar[i].nome);
+                        printf("Data da compra:%02s/%02s/%04s \n", pagar[i].dia, pagar[i].mes, pagar[i].ano);
+                        printf("Valor da compra:%.2f \n", pagar[i].valor);
+                        total_desp = total_desp + pagar[i].valor;
+
+                    }
+                    else if (retorno != 0)
+                        printf("não possui despesas para o mes %s \n", mes);
+                 }
+
+             }
+                if (total_desp > 0)
+                printf("O total de despesas para o mes %s é %.2f\n", mes, total_desp);
+                total_desp = 0;
+            break;
+            }
+        }
+
+    }while (op != 0);
+
+
+
+
+
+    // o usuario consulta as contas a pagar no mes
+
+}
+/*
+
+    do {
+        printf("----------------------------------------\n");
+        printf("------SEJA BEM-VINDO À CLÍNICA A&G------\n");
+        printf("----------------------------------------\n\n");
+        printf("\n---------------CADASTROS-------------------\n\n");
+        printf("[1] CADASTRAR COLABORADOR\n");
+        printf("[2] CADASTRAR MORADOR\n");
+        printf("[3] CADASTRAR VISITANTE\n");
+        printf("\n--------------ALTERAR DADOS----------------\n\n");
+        printf("[4] ALTERAR DADOS DE COLABORADOR\n");
+        printf("[5] ALTERAR DADOS DE MORADOR\n");
+        printf("[6] ALTERAR DADOS DE VISITANTE\n");
+        printf("\n--------------RELATÓRIOS-------------------\n\n");
+        printf("[10] CONTAS A PAGAR\n");
+        printf("[11] CONTAS A RECEBER\n");
+        printf("[12] FATURAMENTO DIÁRIO E MENSAL\n");
+        printf("[12] LUCRO LIQUIDO MES\n");
+        printf("\n--------------BUSCAR---------------------\n\n");
+        printf("[13] BUSCAR VEICULO POR PLACA\n");
+        printf("[14] BUSCAR MORADOR POR NOME\n");
+        printf("[14] BUSCAR COLABORADOR POR NOME\n");
+        printf("\n---------------------------------------\n\n");
+        printf("[15] AVALIE NOSSO ATENDIMENTO\n");
+        printf("[0] SAIR \n");
+        printf("\nEscolha uma opção:");
+        scanf("%d", &op);
+
+*/
